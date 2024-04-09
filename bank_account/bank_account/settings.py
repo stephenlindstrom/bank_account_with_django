a485513fe29f86125e2ba7a1e05c93ac3c6bcf7a
Stephen Lindstrom <stphnlindstrom@gmail.com>
2024-04-08 20:19:41 -0700

    Add max_digits to balance in Account model

--- bank_account/bank_account/settings.py
+++ bank_account/bank_account/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'banking_app.apps.BankingAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
