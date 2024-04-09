2954d80e78aa4802cc9688cc639c24d5a13e4841
Stephen Lindstrom <stphnlindstrom@gmail.com>
2024-04-09 16:00:19 -0700

    Update index view to return list of first names in accounts

--- bank_account/banking_app/views.py
+++ bank_account/banking_app/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
 
+from .models import Account
+
 def index(request):
-    return HttpResponse("You're at the banking app index.")
+    account_list  = Account.objects.all()
+    output = ", ".join([account.first_name for account in account_list])
+    return HttpResponse(output)
 
 def balance(request, account_id):
     return HttpResponse("You're looking at account %s." % account_id)
