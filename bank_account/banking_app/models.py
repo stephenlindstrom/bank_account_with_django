b18bd74b0d25fcf807bcecce742e0d3eabd5b129
Stephen Lindstrom <stphnlindstrom@gmail.com>
2024-04-08 20:43:36 -0700

    Create balance view and add url to path

--- bank_account/banking_app/models.py
+++ bank_account/banking_app/models.py
@@ -4,3 +4,6 @@ class Account(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     balance = models.DecimalField(max_digits=14, decimal_places=2, default=0)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
--- bank_account/banking_app/views.py
+++ bank_account/banking_app/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("You're at the banking app index.")
+
+def balance(request, account_id):
+    return HttpResponse("You're looking at account %s." % account_id)
