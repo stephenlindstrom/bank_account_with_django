be0cda6e71f4c4142587d31acf36e70fdd3ca208
Stephen Lindstrom <stphnlindstrom@gmail.com>
2024-04-08 20:23:02 -0700

    Update max digits for balance in account

--- bank_account/banking_app/models.py
+++ bank_account/banking_app/models.py
@@ -3,4 +3,4 @@ from django.db import models
 class Account(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    balance = models.DecimalField(max_digits=None, decimal_places=2, default=0)
+    balance = models.DecimalField(max_digits=14, decimal_places=2, default=0)
