6350569dfc3d0432f9218eff3f278ef01b5624fc
Stephen Lindstrom <stphnlindstrom@gmail.com>
2024-04-09 18:15:42 -0700

    Display account first names as list using templates

--- bank_account/banking_app/views.py
+++ bank_account/banking_app/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Account
 
 def index(request):
     account_list  = Account.objects.all()
-    output = ", ".join([account.first_name for account in account_list])
-    return HttpResponse(output)
+    template = loader.get_template("banking_app/index.html")
+    context = {"account_list": account_list,}
+    return HttpResponse(template.render(context, request))
 
 def balance(request, account_id):
     return HttpResponse("You're looking at account %s." % account_id)
