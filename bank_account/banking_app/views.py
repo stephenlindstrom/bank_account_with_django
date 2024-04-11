266caf82cd8a676f00ce19acb3aee11f20ef6b9a
Stephen Lindstrom <stphnlindstrom@gmail.com>
2024-04-11 13:13:56 -0700

    Add visit counter using session framework

--- bank_account/banking_app/views.py
+++ bank_account/banking_app/views.py
@@ -5,8 +5,16 @@ from .models import Account
 
 def index(request):
     account_list  = Account.objects.all()
+
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     template = loader.get_template("banking_app/index.html")
-    context = {"account_list": account_list,}
+    context = {
+                "account_list": account_list,
+                "num_visits": num_visits,
+               }
+
     return HttpResponse(template.render(context, request))
 
 def balance(request, account_id):
