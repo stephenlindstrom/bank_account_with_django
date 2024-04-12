a6041635339eb2fbc97feaf4ab285a9335098476
Stephen Lindstrom <stphnlindstrom@gmail.com>
2024-04-12 07:53:48 -0700

    Make registration form and pass data to new registered view

--- bank_account/banking_app/urls.py
+++ bank_account/banking_app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("admin/", admin.site.urls),
     path("<int:account_id>/balance/", views.balance, name="balance"),
+    path("register/", views.register, name="register"),
+    path("registered/", views. registered, name="registered"),
 ]
--- bank_account/banking_app/views.py
+++ bank_account/banking_app/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.template import loader
 
 from .models import Account
@@ -11,13 +12,20 @@ def index(request):
     num_visits = request.session.get('num_visits', 1)
     request.session['num_visits'] = num_visits + 1
 
-    template = loader.get_template("banking_app/index.html")
     context = {
                 "account": account,
                 "num_visits": num_visits,
                }
 
-    return HttpResponse(template.render(context, request))
+    return render(request, "banking_app/index.html", context)
 
 def balance(request, account_id):
     return HttpResponse("You're looking at account %s." % account_id)
+
+def register(request):
+    return render(request, "banking_app/register.html")
+
+def registered(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        return HttpResponse(username)
